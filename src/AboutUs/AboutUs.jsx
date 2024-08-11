e823c0e7c8c846f5501dc83ab990503bc284f9da
John Benson <JDBensonWIU@gmail.com>
2024-08-11 14:46:29 -0400

    style fix, About Us Content change gitignore revert - John Benson

--- src/AboutUs/AboutUs.jsx
+++ src/AboutUs/AboutUs.jsx
@@ -4,7 +4,7 @@ import { PiTreeFill } from "react-icons/pi";
 const AboutUs = () => {
     return (
         <div className={classes.about_us_cntnr} style={{ padding: '5%' }}>
-            <p style={{ fontSize: '5cqh', color: '#98FF98'}}>We are Two Brothers, and We Kindly Remove Trees <PiTreeFill /></p>
+            <p style={{ fontSize: '4.5cqh', color: '#98FF98'}}>We are Two Brothers, and We Kindly Remove Trees <PiTreeFill /></p>
             <p style={{ fontSize: '3cqh', fontWeight: 400}}>But, We Can Do So Much More...</p>
             <p style={{ fontSize: '3cqh', fontWeight: 400}}>Be it Furniture, Lumber, Firewood or Mulch.</p>
             <p style={{ fontSize: '3cqh', fontWeight: 400}}>We Convert Your Tree Into Any Form You Choose.</p>
